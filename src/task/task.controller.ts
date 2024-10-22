ef1b2a3b86fa6608b4bac1a55dd4a775a2793b82
Ali Ramazanov <toaliramazanov@gmail.com>
2024-10-22 14:02:32 +0400

    add: create task & get one task endpoints

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,5 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { TaskService } from './task.service';
+import { TaskEntity } from './task.entity';
+import { CreateTaskDto } from './dto/create-task.dto';
 
 @Controller('task')
 export class TaskController {
@@ -10,5 +12,15 @@ export class TaskController {
   @Get()
   getAllTasks() {
     return this.taskService.getAllTasks();
+  }
+
+  @Get(':id')
+  getTask(@Param('id') id: string): Promise<TaskEntity> {
+    return this.taskService.getTask(id);
+  }
+
+  @Post()
+  createTask(@Body() createTaskDto: CreateTaskDto): Promise<TaskEntity> {
+    return this.taskService.createTask(createTaskDto);
   }
 }
--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -1,7 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { TaskEntity } from './task.entity';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { TaskStatus } from 'src/helpers/enum/task-status.enum';
 
 @Injectable()
 export class TaskService {
@@ -12,5 +14,25 @@ export class TaskService {
 
   async getAllTasks() {
     return this.taskRepository.find();
+  }
+
+  async getTask(id: string): Promise<TaskEntity> {
+    const found = await this.taskRepository.findOne({ where: { id } });
+    if (!found) throw new NotFoundException();
+    return found;
+  }
+
+  async createTask(createTaskDto: CreateTaskDto): Promise<TaskEntity> {
+    const { title, description } = createTaskDto;
+    const status = TaskStatus.open;
+
+    const task = this.taskRepository.create({
+      title,
+      description,
+      status,
+    });
+
+    await this.taskRepository.save(task);
+    return task;
   }
 }
